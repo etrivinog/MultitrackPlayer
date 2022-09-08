b60ea190742f10d024d80e49234fd533cbb38c0f
Esteban Triviño <esteban.trivino@globant.com>
2022-09-08 06:14:22 -0500

    Initial Commit

---
+++ Shared/Play_SecuenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_SecuenceApp.swift
+//  Shared
+//
+//  Created by Esteban Rafael Trivino Guerra on 8/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Play_SecuenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
